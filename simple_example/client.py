3dac9e44f91c2e9bb8e55e12454c72b0472a7486
Solomon Eshun <solomoneinsteinmit300@gmail.com>
2025-11-28 01:16:55 +0000

    Implement movie information retrieval with Celery and OpenAI; add Dockerfile, docker-compose, and worker setup

--- simple_example/client.py
+++ simple_example/client.py
@@ -6,7 +6,7 @@ from worker import random_number, app
 
 time.sleep(5)
 
-result_future = random_number.delay(100)
+result_future = random_number.delay(100) # this is used to submit the task. so because of the task decorator, the delay can be called as a method of the random_number function and the arguement is passed. in this eg. 100
 result = AsyncResult(result_future.id, app=app)
 
 print("Submitted task")
